d89c94b0c46ed9667461ea9076f1d147af3db1e9
Yosia Sitompul <ayos.rgt13@gmail.com>
2026-03-02 16:16:10 +0700

    Sedikit perubahan pada read

--- PengajuanSuratTugas/PengajuanSuratTugas_Read.js
+++ PengajuanSuratTugas/PengajuanSuratTugas_Read.js
@@ -5,22 +5,14 @@ function dnew() {
   alert("Apakah anda yakin ingin menambahkan data baru?");
 }
 function goToCreate() {
-    // Berpindah ke file Create
     window.location.href = "PengajuanSuratTugas_Create.html";
 }
 function hapusBaris(button) {
-    // 1. Tampilkan konfirmasi ke user
     const konfirmasi = confirm("Apakah Anda yakin ingin menghapus data ini?");
 
     if (konfirmasi) {
-        // 2. Cari elemen baris (tr) tempat tombol tersebut berada
-        // button -> td -> tr
         const baris = button.parentElement.parentElement;
-        
-        // 3. Hapus baris tersebut dari tabel
         baris.remove();
-
-        // 4. Opsional: Tampilkan pesan sukses
         alert("Data berhasil dihapus.");
     }
 }
